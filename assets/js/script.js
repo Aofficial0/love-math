96bf43480f768b8f5f7854797f3df86e8dfeee0e
Ahmed Mohamed <ahmedtarekaha@gmail.com>
2024-06-21 11:41:53 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,20 +12,29 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You Clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * The main game "loop" called when the script is first loaded 
  * and after the use's answer has been processed 
  */
-function runGame() {
+function runGame(gameType) {
     // creates two random numbers between 1 and 25 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 
@@ -48,7 +57,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+"
 
 
 }
